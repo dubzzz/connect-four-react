64c8c23a663f9133022bc017b3d30d3a65eb3701
Nicolas DUBIEN <github@dubien.org>
2018-11-01 00:17:17 +0100

    Add instructions

--- src/ConnectFourApp.tsx
+++ src/ConnectFourApp.tsx
@@ -3,11 +3,13 @@ import './ConnectFourApp.css';
 
 import Board from './components/Board';
 import Controls from './components/Controls';
+import Instructions from './components/Instructions';
 
 class ConnectFourApp extends Component {
   render() {
     return (
       <div className="connect-four-app">
+        <Instructions />
         <Board />
         <Controls />
       </div>
---
+++ src/components/Instructions.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './Instructions.css';
+import { bindActionCreators, Dispatch, Action } from 'redux';
+import { ReduxState } from '../redux/reducers';
+import { Player } from '../redux/models/player';
+
+interface Props extends StateProps, DispatchProps {}
+type State = {};
+
+class Instructions extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    switch (this.props.currentPlayer) {
+      case Player.PlayerA:
+        return <div className="instructions player-1">Player #1 turn</div>;
+      case Player.PlayerB:
+        return <div className="instructions player-2">Player #2 turn</div>;
+    }
+  }
+}
+
+function mapStateToProps(state: ReduxState) {
+  return { currentPlayer: state.connectFour.currentPlayer };
+}
+type StateProps = ReturnType<typeof mapStateToProps>;
+
+function mapDispatchToProps(dispatch: Dispatch<Action>) {
+  return { ...bindActionCreators({}, dispatch) };
+}
+type DispatchProps = ReturnType<typeof mapDispatchToProps>;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Instructions);
