bc1756cd24df3a63e6ce1a6095f3e63a27939171
Nicolas DUBIEN <github@dubien.org>
2018-11-01 02:24:43 +0100

    Add redux-devtools-extension

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, devToolsEnhancer({}));
