b2e9b1b5a19a21ec10ced5da09bfa405b7c6b654
Nicolas DUBIEN <github@dubien.org>
2018-11-01 13:47:24 +0100

    Add PLAY_AT tests

--- src/redux/reducers/connectFour.test.tsx
+++ src/redux/reducers/connectFour.test.tsx
@@ -1,7 +1,15 @@
 import * as fc from 'fast-check';
 import reducer, { DefaultDimensions } from './connectFour';
-import { anyStateArb } from '../../test-arbitraries/reduxState';
+import {
+  anyStateArb,
+  playableStateArb,
+  playableNoVictoryStateArb,
+  playableVictoryStateArb,
+  gameOverStateArb,
+  notPlayableStateArb
+} from '../../test-arbitraries/reduxState';
 import { Player } from '../../models/player';
+import { playToken } from './grid';
 
 describe('NEW_GAME', () => {
   it('Should create a new grid', () =>
@@ -27,6 +35,52 @@ describe('NEW_GAME', () => {
       fc.property(anyStateArb, state => {
         const nextSubState = reducer(state.connectFour, { type: 'NEW_GAME', payload: null });
         return nextSubState.currentPlayer === state.connectFour.currentPlayer;
+      })
+    ));
+});
+
+describe('PLAY_AT', () => {
+  it('Should always switch to another valid player', () =>
+    fc.assert(
+      fc.property(playableStateArb, ({ state, selectedColumn }) => {
+        const nextSubState = reducer(state.connectFour, { type: 'PLAY_AT', payload: { columnIdx: selectedColumn } });
+        return (
+          nextSubState.currentPlayer !== state.connectFour.currentPlayer && nextSubState.currentPlayer !== Player.None
+        );
+      })
+    ));
+  it('Should not impact winner if no victory', () =>
+    fc.assert(
+      fc.property(playableNoVictoryStateArb, ({ state, selectedColumn }) => {
+        const nextSubState = reducer(state.connectFour, { type: 'PLAY_AT', payload: { columnIdx: selectedColumn } });
+        return nextSubState.winner === Player.None;
+      })
+    ));
+  it('Should set winner on victory', () =>
+    fc.assert(
+      fc.property(playableVictoryStateArb, ({ state, selectedColumn }) => {
+        const nextSubState = reducer(state.connectFour, { type: 'PLAY_AT', payload: { columnIdx: selectedColumn } });
+        return nextSubState.winner === state.connectFour.currentPlayer;
+      })
+    ));
+  it('Should add the token into the grid', () =>
+    fc.assert(
+      fc.property(playableStateArb, ({ state, selectedColumn }) => {
+        const nextSubState = reducer(state.connectFour, { type: 'PLAY_AT', payload: { columnIdx: selectedColumn } });
+        const expectedGrid = playToken(state.connectFour.grid, selectedColumn, state.connectFour.currentPlayer);
+        expect(nextSubState.grid).toEqual(expectedGrid);
+      })
+    ));
+  it('Should throw if the column is full', () =>
+    fc.assert(
+      fc.property(notPlayableStateArb, ({ state, selectedColumn }) => {
+        expect(() => reducer(state.connectFour, { type: 'PLAY_AT', payload: { columnIdx: selectedColumn } })).toThrow();
+      })
+    ));
+  it('Should throw if the game is over', () =>
+    fc.assert(
+      fc.property(gameOverStateArb, fc.nat(DefaultDimensions.width - 1), (state, selectedColumn) => {
+        expect(() => reducer(state.connectFour, { type: 'PLAY_AT', payload: { columnIdx: selectedColumn } })).toThrow();
       })
     ));
 });
--- src/test-arbitraries/reduxState.tsx
+++ src/test-arbitraries/reduxState.tsx
@@ -1,7 +1,17 @@
 import * as fc from 'fast-check';
 import { ReduxState } from '../redux/reducers';
-import { gridArb } from './grid';
-import { playerOrNoneArb } from './player';
+import {
+  gridArb,
+  topRightDiagonalVictoryPlayOnGridArb,
+  lineVictoryPlayOnGridArb,
+  columnVictoryPlayOnGridArb,
+  topLeftDiagonalVictoryPlayOnGridArb,
+  noVictoryPlayOnGridArb,
+  playOnFullColumnGridArb
+} from './grid';
+import { playerOrNoneArb, playerArb } from './player';
+import { Player } from '../models/player';
+import { playToken } from '../redux/reducers/grid';
 
 export const anyStateArb: fc.Arbitrary<ReduxState> = fc.record({
   connectFour: fc.record({
@@ -10,3 +20,57 @@ export const anyStateArb: fc.Arbitrary<ReduxState> = fc.record({
     currentPlayer: playerOrNoneArb
   })
 });
+
+export const notPlayableStateArb = fc
+  .tuple(playOnFullColumnGridArb, playerArb)
+  .map(([{ grid, selectedColumn }, player]) => {
+    const state: ReduxState = {
+      connectFour: {
+        grid,
+        currentPlayer: player,
+        winner: Player.None
+      }
+    };
+    return { state, selectedColumn };
+  });
+export const playableVictoryStateArb = fc
+  .oneof(
+    lineVictoryPlayOnGridArb,
+    columnVictoryPlayOnGridArb,
+    topLeftDiagonalVictoryPlayOnGridArb,
+    topRightDiagonalVictoryPlayOnGridArb
+  )
+  .map(({ grid, selectedColumn, player }) => {
+    const state: ReduxState = {
+      connectFour: {
+        grid,
+        currentPlayer: player,
+        winner: Player.None
+      }
+    };
+    return { state, selectedColumn };
+  });
+export const playableNoVictoryStateArb = noVictoryPlayOnGridArb.map(({ grid, selectedColumn, player }) => {
+  const state: ReduxState = {
+    connectFour: {
+      grid,
+      currentPlayer: player,
+      winner: Player.None
+    }
+  };
+  return { state, selectedColumn };
+});
+
+export const playableStateArb = fc.oneof(playableVictoryStateArb, playableNoVictoryStateArb);
+
+export const gameOverStateArb = fc
+  .tuple(playableVictoryStateArb, playerArb)
+  .map(([{ state, selectedColumn }, randomPlayer]) => {
+    return {
+      connectFour: {
+        grid: playToken(state.connectFour.grid, selectedColumn, state.connectFour.currentPlayer),
+        winner: state.connectFour.currentPlayer,
+        currentPlayer: randomPlayer // we could have just changed the player to move the the next one but setting a purely random one might cover additional cases (maybe useful if we add some king of penalty forbidding the player to play if she/he did something wrong..)
+      }
+    };
+  });
