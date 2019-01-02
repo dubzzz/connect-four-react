4d5c44cf5d5d908058a9f3583ccf94847d57d28f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-02 10:20:26 +0000

    Deploy dubzzz/connect-four-react to github.com/dubzzz/connect-four-react.git:gh-pages

---
+++ bug/precache-manifest.ddd0effc8a057959fd4c5eeceaa77372.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a72d5870f0dcdd025d9",
+    "url": "/connect-four-react/static/js/runtime~main.6a72d587.js"
+  },
+  {
+    "revision": "ec2c3aa6aea2fe00ddff",
+    "url": "/connect-four-react/static/js/main.ec2c3aa6.chunk.js"
+  },
+  {
+    "revision": "c98a5955a258d7efe7ba",
+    "url": "/connect-four-react/static/js/1.c98a5955.chunk.js"
+  },
+  {
+    "revision": "ec2c3aa6aea2fe00ddff",
+    "url": "/connect-four-react/static/css/main.501ace8b.chunk.css"
+  },
+  {
+    "revision": "418ce5afa63c6069804d2c004db88ca9",
+    "url": "/connect-four-react/index.html"
+  }
+];
